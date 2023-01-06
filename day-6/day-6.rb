dfae1366520df14f5cc19cb281cde8f353536b9d
Patrycja Miekina <patrycja.miekina@madetech.com>
2023-01-06 11:50:03 +0000

    Day 6 logic complete

--- day-6/day-6.rb
+++ day-6/day-6.rb
@@ -1,5 +1,30 @@
 require './day-6-input'
-# 1. split the string into an array
-# 2. start with the first 4 symbols -> push into a new string
-# 3. Check if unique string -> four_chars.length == four_chars.uniq.length
-# 4. If not -> shift the first bit and add the new bit
+
+class CommsSignal
+    attr_accessor :current_four_chars, :marker_index
+
+    def initialize(string)
+        @signal = string
+        @current_four_chars = string[0,4].split("")
+        @marker_index = 4
+    end
+
+    def all_four_chars_unique?
+        @current_four_chars.length == @current_four_chars.uniq.length ? true : false
+    end
+
+    def check_signal_for_four_unique_chars
+        return true if self.all_four_chars_unique?
+        @signal.split("").each_with_index do |char, index|
+            next if index <= 3
+            @marker_index += 1
+            @current_four_chars.shift
+            @current_four_chars << char
+            return true if self.all_four_chars_unique?
+        end
+    end
+
+    def return_signal_marker
+        @marker_index
+    end
+end
--- day-6/spec/day-6_spec.rb
+++ day-6/spec/day-6_spec.rb
@@ -1,2 +1,35 @@
 require_relative '../day-6'
 require_relative '../day-6-input'
+
+describe "CommsSignal" do
+    signal = CommsSignal.new(TEST_SIGNAL)
+    signal1 = CommsSignal.new("bvwb")
+    signal2 = CommsSignal.new("abcd")
+
+    context "#all_four_chars_unique?" do
+        it "returns false if the four consequent characters are not unique" do
+            expect(signal1.all_four_chars_unique?).to be(false)
+        end
+
+        it "returns true if the four consequent characters are unique" do
+            expect(signal2.all_four_chars_unique?).to be(true)
+        end
+    end
+
+    context "#check_signal_for_four_unique_chars" do
+        it "removes the first character and adds the next to the checked four if not unique" do
+            signal.check_signal_for_four_unique_chars
+            expect(signal.current_four_chars).to eq(["v", "w", "b", "j"])
+        end
+
+        it "returns true when all 4 characters unique" do
+            expect(signal.check_signal_for_four_unique_chars).to eq(true)
+        end
+    end
+
+    context "#return_signal_marker" do
+        it "returns the character that marks the beginning of the correct signal" do
+            expect(signal.return_signal_marker).to eq(5)
+        end
+    end
+end
