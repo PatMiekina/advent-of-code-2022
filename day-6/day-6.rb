7a43de9f8cce4a83cfd9d4cb13fdffc0fc804d9d
Patrycja Miekina <patrycja.miekina@madetech.com>
2023-01-06 11:17:45 +0000

    Day 6 setup

---
+++ day-6/day-6.rb
@@ -0,0 +1,5 @@
+require './day-6-input'
+# 1. split the string into an array
+# 2. start with the first 4 symbols -> push into a new string
+# 3. Check if unique string -> four_chars.length == four_chars.uniq.length
+# 4. If not -> shift the first bit and add the new bit
---
+++ day-6/spec/day-6_spec.rb
@@ -0,0 +1,2 @@
+require_relative '../day-6'
+require_relative '../day-6-input'
